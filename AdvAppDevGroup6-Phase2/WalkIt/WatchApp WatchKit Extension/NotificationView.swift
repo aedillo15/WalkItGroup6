71e1d7274de5f031409ad6efe43f7bcd59f0a2cf
Ethan Reinke <ethanreinke2000@gmail.com>
2021-12-05 18:46:01 -0500

    Added WatchOS application

---
+++ AdvAppDevGroup6-Phase2/WalkIt/WatchApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by user on 2021-12-05.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
